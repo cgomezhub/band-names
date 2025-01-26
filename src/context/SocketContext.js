2f038996ce1cdd920eb6e27122150297bb4bf928
Carlos Gomez <cgomezlugo@hotmail.com>
2025-01-26 18:13:37 -0400

    feat: port changes

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -5,7 +5,8 @@ import { useSocket } from "../hooks/useSocket";
 export const SocketContext = createContext();
 
 export const SockectProvider = ({ children }) => {
-  const { socket, online } = useSocket("http://localhost:8080");
+  // const { socket, online } = useSocket("http://localhost:8080");
+  const { socket, online } = useSocket("band-names-server-production-88d3.up.railway.app");
   return (
     <SocketContext.Provider value={{ socket, online }}>
       {children}
